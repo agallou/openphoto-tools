fde7120823e299cf537ea2eb7a6eb103efc2a3f3
Adrien Gallou <adriengallou@gmail.com>
2012-02-06 13:51:53 +0100

    moved oauth config to config

--- config/config.php
+++ config/config.php
@@ -1,3 +1,9 @@
 <?php
-$config             = array();
-$config['exiftran'] = dirname(__FILE__) . '/../vendor/exiftran/exiftran';
+$config                    = array();
+$config['exiftran']        = dirname(__FILE__) . '/../vendor/exiftran/exiftran';
+$config['consumer_key']    = getenv('consumerKey');
+$config['consumer_secret'] = getenv('consumerSecret');
+$config['token']           = getenv('token');
+$config['token_secret']    = getenv('tokenSecret');
+$config['host']            = 'openphoto.adrien-gallou.fr';
+
